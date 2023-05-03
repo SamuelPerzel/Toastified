81c4df4605c760a7d7f53dfc4c2a43b499c2ad5c
SamuelPerzel <perzelsamuel0@gmail.com>
2023-05-03 14:23:06 +0200

    Edited GlowrayTrunkPlacer.java for easier use.

--- src/main/java/samuelperzel/toastified/world/gen/trunk/GlowrayTrunkPlacer.java
+++ src/main/java/samuelperzel/toastified/world/gen/trunk/GlowrayTrunkPlacer.java
@@ -17,6 +17,13 @@ import java.util.List;
 import java.util.function.BiConsumer;
 
 public class GlowrayTrunkPlacer extends TrunkPlacer {
+    private TestableWorld testableWorld;
+    private BiConsumer<BlockPos, BlockState> biConsumer;
+    private Random random;
+    private BlockPos.Mutable mutable;
+    private TreeFeatureConfig treeFeatureConfig;
+    private BlockPos blockPos;
+
     public static final Codec<GlowrayTrunkPlacer> CODEC = RecordCodecBuilder.create(instance -> fillTrunkPlacerFields(instance).apply(instance, GlowrayTrunkPlacer::new));
 
     public GlowrayTrunkPlacer(int baseHeight, int firstRandomHeight, int secondRandomHeight) {
@@ -30,17 +37,30 @@ public class GlowrayTrunkPlacer extends TrunkPlacer {
 
     @Override
     public List<FoliagePlacer.TreeNode> generate(TestableWorld world, BiConsumer<BlockPos, BlockState> replacer, Random random, int height, BlockPos startPos, TreeFeatureConfig config) {
+        // class variables
+        this.testableWorld = world;
+        this.biConsumer = replacer;
+        this.random = random;
+        this.mutable = new BlockPos.Mutable();
+        this.treeFeatureConfig = config;
+        this.blockPos = startPos;
+
         setToDirt(world, replacer, random, startPos.down(), config);
 
-        BlockPos.Mutable mutable = new BlockPos.Mutable();
-
-        for (int i = 0; i < height; i++) {
-            this.setLog(world, replacer, random, mutable, config, startPos, 0, i, 0);
+        for (int y = 0; y < 2*height; y++) {
+            // TODO build tree here
+            setLog(0, y, 0);
         }
 
-        return ImmutableList.of(new FoliagePlacer.TreeNode(startPos.up(height - 2), 0, false));
+        return ImmutableList.of(new FoliagePlacer.TreeNode(startPos.up(2*height), 0, false));
     }
 
+    // shorter, easier to use, setLog method (calls setLog which actually places logs)
+    private void setLog(int x, int y, int z) {
+        setLog(this.testableWorld, this.biConsumer, this.random, this.mutable, this.treeFeatureConfig, this.blockPos, x, y, z);
+    }
+
+    // longer setLog method (actually sets the log)
     private void setLog(TestableWorld testableWorld, BiConsumer<BlockPos, BlockState> biConsumer, Random random, BlockPos.Mutable mutable, TreeFeatureConfig treeFeatureConfig, BlockPos blockPos, int x, int y, int z) {
         mutable.set(blockPos, x, y, z);
         this.trySetState(testableWorld, biConsumer, random, mutable, treeFeatureConfig);
